bbc7378b8409f03331b8cfae50453275ed6e4bb3
alikastrati <93733635+alikastrati@users.noreply.github.com>
2024-11-13 18:33:25 +0100

    Forgot files

---
+++ api/routes/UserRoute.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+
+
+router.get('/', async(req, res) => {
+  const listAllUsers = await User.findAll();
+  res.json(listAllUsers);
+});
+
+router.post('/register', async(req, res) => {
+  const {name, surname, email, password} = req.body;
+
+  try {
+    const existingUser = await User.findOne({where: {email}});
+    if (existingUser) {
+      return res.status(400).json({error: 'Email is already registered'});
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name, 
+      surname,
+      email, 
+      password: hashedPassword
+    });
+
+    res.json(201).json(newUser);
+
+  } catch(error) {
+    console.error("Error occured during registration", error);
+    res.status(500).json({error: 'Internal Server Error'});
+
+  }
+});
+
+
+module.exports = router;
+
+
