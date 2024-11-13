50f69e12062046cbbaaac30d2b7a3f08fd7ed475
alikastrati <93733635+alikastrati@users.noreply.github.com>
2024-11-13 18:32:51 +0100

    User Route API testing

--- api/server.js
+++ api/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const db = require('./models/');
+const cors = require('cors');
 const app = express();
 
 const PORT = 3001;
 
 
-app.listen(PORT, 'localhost', () => {
-  console.log(">Listening on port" );
-})
+// Parse JSON request
+app.use(express.json());
 
+// Whitelist aPI
+app.use(cors());
+
+// User Router
+const userRouter = require('./routes/UserRoute');
+app.use('/', userRouter);
+
+
+db.sequelize.sync().then( () => {
+  app.listen(PORT, 'localhost', () => {
+    console.log(">Listening on port 3001");
+
+  })
+})
